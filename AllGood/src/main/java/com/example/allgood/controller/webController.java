f24bb0d6026aff7b36053d66c977d1b03582eda1
Emilyfanf <jifanw@student.unimelb.edu.au>
2022-09-25 22:04:08 +1000

    initial commit

---
+++ AllGood/src/main/java/com/example/allgood/controller/webController.java
@@ -0,0 +1,19 @@
+package com.example.allgood.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class webController {
+    @RequestMapping("/HomePage")
+    public String homePage() {
+        return "HomePage";
+    }
+
+    @RequestMapping("/GMlogin")
+    public String login2() {
+        return "login-2";
+    }
+    
+}
