e8846e60024a230c022791c9ee1e9cd1f2a07ca7
ShengZeng117 <101320142+ShengZeng117@users.noreply.github.com>
2022-10-18 19:00:39 +1100

    Delete allgood directory

--- allgood/controllers/managerController.js
+++
@@ -1,8 +0,0 @@
-const Manager = require('../models/Manager')
-const Device = require('../models/Devices')
-
-/*const logIn = (req, res) => {
-    res.render('staff_login.hbs', { layout: 'staff_loginD', message: req.flash('message') })
-}*/
-
-module.exports = {}
