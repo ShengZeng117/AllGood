3f197d614da505d7f232be31aade746346d4e0cc
HuiyiWang1 <huiywang1@student.unimelb.edu.au>
2022-10-19 00:32:38 +1100

    update login

--- allgood/controllers/managerController.js
+++ allgood/controllers/managerController.js
@@ -1,8 +1,10 @@
 const Manager = require('../models/Manager')
 const Device = require('../models/Devices')
 
-/*const logIn = (req, res) => {
-    res.render('staff_login.hbs', { layout: 'staff_loginD', message: req.flash('message') })
-}*/
+const managerLogin = (req, res) => {
+    res.render('generalManager_login.hbs', { layout: 'generalManager_loginD', message: req.flash('message') })
+}
 
-module.exports = {}
+module.exports = {
+    managerLogin
+}
