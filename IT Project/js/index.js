6a9c98dd1ed6f48a2a347053fa88746405b7d37e
ShengZeng117 <101320142+ShengZeng117@users.noreply.github.com>
2022-10-20 23:15:20 +1100

    Add files via upload

---
+++ IT Project/js/index.js
@@ -0,0 +1,201 @@
+
+
+
+
+function show(anything){
+    document.querySelector('.textBox').value = anything;
+}
+
+
+
+let changePassword = document.getElementById("changePassword");
+let lowerCase = document.getElementById('lower');
+let upperCase = document.getElementById('upper');
+let numberCase = document.getElementById('number');
+let minLength = document.getElementById('length');
+let specialCase = document.getElementById('special');
+let passOld = document.getElementById('passOld');
+let failOld = document.getElementById('failOld');
+let passNew = document.getElementById('passNew');
+let failNew = document.getElementById('failNew');
+let passCon = document.getElementById('passCon');
+let failCon = document.getElementById('failCon');
+let newPassword = document.getElementById('newPassword');
+let pwd;
+
+let  overview = document.getElementById("overview");
+let  usage = document.getElementById("usage");
+let  datas = document.getElementById("datas");
+let  profile = document.getElementById("profile");
+let changePass = document.getElementById("changePass");
+
+let over = document.getElementById("over");
+let usag = document.getElementById("usag");
+let data = document.getElementById("data");
+let prof = document.getElementById("prof");
+let changeSucc = document.getElementById("changeSucc");
+let backToProf = document.getElementById("backToProf");
+
+
+let btn = document.getElementById('btn');
+
+
+
+function removeAllLeft(){
+    overview.style.backgroundColor = "#3D5967";
+    overview.style.color = "white";
+    usage.style.backgroundColor = "#3D5967";
+    usage.style.color = "white";
+    datas.style.backgroundColor = "#3D5967";
+    datas.style.color = "white";
+    profile.style.backgroundColor = "#3D5967";
+    profile.style.color = "white";
+}
+
+overview.onclick = function(){
+    over.style.display = "flex";
+    prof.style.display = "none";
+    data.style.display = "none";
+    usag.style.display = "none";
+    changeSucc.style.display = "none";
+    changePassword.style.display = "none";
+    removeAllLeft();
+    overview.style.backgroundColor = "#F4F2F2";
+    overview.style.color = "black";
+}
+
+usage.onclick = function(){
+    usag.style.display = "block";
+    prof.style.display = "none";
+    data.style.display = "none";
+    over.style.display = "none";
+    changeSucc.style.display = "none";
+    changePassword.style.display = "none";
+    removeAllLeft();
+    this.style.backgroundColor = "#F4F2F2";
+    this.style.color = "black";
+}
+datas.onclick = function(){
+    data.style.display = "flex";
+    prof.style.display = "none";
+    over.style.display = "none";
+    usag.style.display = "none";
+    changeSucc.style.display = "none";
+    changePassword.style.display = "none";
+    removeAllLeft();
+    this.style.backgroundColor = "#F4F2F2";
+    this.style.color = "black";
+}
+profile.onclick = function(){
+    prof.style.display = "flex";
+    over.style.display = "none";
+    data.style.display = "none";
+    usag.style.display = "none";
+    changeSucc.style.display = "none";
+    changePassword.style.display = "none";
+    removeAllLeft();
+    this.style.backgroundColor = "#F4F2F2";
+    this.style.color = "black";
+}
+
+changePass.onclick = function(){
+    prof.style.display = "none";
+    changePassword.style.display = "flex";
+}
+
+btn.onclick = function(){
+    changePassword.style.display = "none";
+    changeSucc.style.display = "flex";
+}
+backToProf.onclick = function(){
+    changeSucc.style.display = "none";
+    prof.style.display = "flex";
+}
+
+
+
+
+const pPattern = /^.*(?=.{8,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/;
+//check password
+function checkPassword(data){
+    pwd = data;
+    const lower = new RegExp('(?=.*[a-z])')
+    const upper = new RegExp('(?=.*[A-Z])')
+    const number = new RegExp('(?=.*[0-9])')
+    const length = new RegExp('(?=.{8,})')
+    const special = new RegExp('(?=.*[!@#\$%\^&\*])')
+
+    //check lowercase letter
+    if (lower.test(data)) {
+        lowerCase.classList.add('valid')
+    } else {
+        lowerCase.classList.remove('valid')
+    }
+    //check uppercase letter
+    if (upper.test(data)) {
+        upperCase.classList.add('valid')
+    } else {
+        upperCase.classList.remove('valid')
+    }
+    //check number
+    if (number.test(data)) {
+        numberCase.classList.add('valid')
+    } else {
+        numberCase.classList.remove('valid')
+    }
+    //check special
+    if (special.test(data)) {
+        specialCase.classList.add('valid')
+    } else {
+        specialCase.classList.remove('valid')
+    }
+    //check length
+    if (length.test(data)) {
+        minLength.classList.add('valid')
+    } else {
+        minLength.classList.remove('valid')
+    }
+
+    if (pPattern.test(data)){
+        passNew.style.display = 'inline-block';
+        failNew.style.display = 'none';
+    }else{
+        failNew.style.display = 'inline-block';
+        passNew.style.display = 'none';
+    }
+    console.log(pwd);
+}
+
+function recheckPassword(data){
+    if(data==pwd){
+        passCon.style.display = ('inline-block');
+        failCon.style.display = ('none');
+    }else{
+        failCon.style.display = ('inline-block');
+        passCon.style.display = ('none');
+    }
+    if(passNew.style.display == 'inline-block' && passCon.style.display == 'inline-block'){
+        btn.removeAttribute("disabled");
+    }
+}
+
+
+let typeFilter = document.getElementById('type');
+
+let resetFilter = document.getElementById('resetFilter');
+resetFilter.onclick = function(){
+   typeFilter.value = 'all';
+}
+
+
+let table = document.getElementById('dataOverview');
+let totalUsage = document.getElementById('totalUsage');
+let totalRow = table.rows.length-1;
+let sum = 0;
+for(var i=1; i<totalRow; i++){
+    sum += Number(table.rows[i].cells[4].innerHTML);
+    totalUsage.innerHTML = sum;
+}
+
+
+
